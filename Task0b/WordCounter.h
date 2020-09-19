d9939ea60dc4ab9a814ddee88403b113e2ce4926
feriter <37873065+feriter@users.noreply.github.com>
2020-09-19 05:37:59 +0300

    Fix remark

--- Task0b/WordCounter.h
+++ Task0b/WordCounter.h
@@ -6,19 +6,14 @@
 #include <iostream>
 
 namespace CSV {
-    bool CompareFunc(std::pair <std::string, int> p1, std::pair <std::string, int> p2);
-
-    bool IsLetterOrNumber(char c);
-
     class WordCounter {
         std::map <std::string, int> wordsCount;
-        unsigned int wordVariety;
         unsigned int wordCount;
         unsigned int maxWordLength;
 
     public:
-        void CountWords(std::string fileName = "input.txt");
-        void MakeCSVFile(std::string fileName = "output.csv");
+        void CountWords(std::string filePath);
+        void MakeCSVFile(std::string filePath);
         WordCounter() { wordVariety = 0; maxWordLength = 0; wordCount = 0; }
         ~WordCounter() = default;
     };
